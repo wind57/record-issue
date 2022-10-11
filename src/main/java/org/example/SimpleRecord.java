3ffc8f2e37a4d3b306d44585b46780b573328cbd
wind57 <eugen.rabii@gmail.com>
2022-10-11 10:46:06 +0300

    first

--- src/main/java/org/example/SimpleRecord.java
+++ src/main/java/org/example/SimpleRecord.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 
 @ConfigurationProperties("retry")
-public record SimpleRecord(@DefaultValue("6") int maxAttempts) {
+public record SimpleRecord(@DefaultValue("5") int maxAttempts) {
 
     @ConstructorBinding
     public SimpleRecord {

fa2a79bdd4596a835f141387fd0916fffa617f38
wind57 <eugen.rabii@gmail.com>
2022-10-23 11:31:30 +0300

    cleanup

--- src/main/java/record/binding/SampleProperties.java
+++ src/main/java/record/binding/SampleProperties.java
@@ -1,8 +1,6 @@
 package record.binding;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
 
 import java.util.List;
 
--- src/test/java/record/binding/Application.java
+++ src/test/java/record/binding/Application.java
@@ -1,9 +1,7 @@
 package record.binding;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(SampleProperties.class)
 @SpringBootApplication
 public class Application {
 }

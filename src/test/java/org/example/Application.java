ece1770d044b4cd05995eda247bab30f1c4fd9ed
wind57 <eugen.rabii@gmail.com>
2022-10-23 12:47:40 +0300

    minimize example

--- src/test/java/org/example/Application.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-@EnableConfigurationProperties(SimpleRecord.class)
-@SpringBootApplication
-public class Application {
-}
